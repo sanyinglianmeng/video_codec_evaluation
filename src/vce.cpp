c37008bc47599338bc4aaf305a4f1c5e423b7612
liuyizhuo <liuyizhuo@baidu.com>
2020-03-04 15:01:58 +0800

    [modify vce]

--- src/vce.cpp
+++ src/vce.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <map>
+#include <set>
 #include <stdio.h>
 #include <string>
 
 #include "lib/command1.h"
+#include "utils/cmdlineutils.h"
 
 namespace vce {
 
@@ -52,10 +54,34 @@ REGISTER(Command1);
 
 } // namespace vce
 
-int main(int argc, const char *argv[]) {
+int main(int argc, char *argv[]) {
+
+    // vce支持的命令集合
+    std::set<std::string> set_vce_all_orders = {"vpsnr", "checkdropframe", "ssim", "msssim", "siti"};
+    // 避免初始化语句太长了，如果要新加的工具，也可以在这里insert
+    // vce_orders.insert("siti");
+
+    // set转string set_vce_all_orders->str_vce_all_orders
+    std::string str_vce_all_orders = "";
+    std::string seperator = ", ";
+    for (auto &order : set_vce_all_orders) {
+        str_vce_all_orders += order + seperator;
+    }
+
+    // 根据str_vce_all_orders构造str_vce_cmdhelp
+    std::string str_vce_cmdhelp = "vce command: ";
+    str_vce_cmdhelp += str_vce_all_orders;
+
+    cmdline::parser cmdPara;
+    cmdPara.add<std::string>("cmd", 'c', str_vce_cmdhelp, true, "");
+    cmdPara.parse_check(argc, argv);
+
+    std::string str_command = cmdPara.get<std::string>("cmd");
+
     vce::Base *p = NULL;
 
-    p = (vce::Base *)vce::VceCommandsFactory::getInstance().CreateObjectByName("Command1");
+    // todo: str_command合法性检查，是否在set_vce_all_orders中
+    p = (vce::Base *)vce::VceCommandsFactory::getInstance().CreateObjectByName(str_command);
     p->print();
 
     return 0;
