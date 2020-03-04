2cd6f576aa3859e2424cf5253790f0c0e9b79795
liuyizhuo <liuyizhuo@baidu.com>
2020-03-04 09:19:07 +0800

    [clang-format]

--- src/vce.cpp
+++ src/vce.cpp
@@ -1,33 +1,27 @@
 #include <iostream>
-#include <stdio.h>
-#include <iostream>
-#include <string>
 #include <map>
+#include <stdio.h>
+#include <string>
 
 #include "lib/command1.h"
 
-namespace vce
-{
+namespace vce {
 
 typedef void *(*PCreateObject)(void);
 
-#define REGISTER(className)                      \
-    className *objectCreator##className()        \
-    {                                            \
-        return new className;                    \
-    }                                            \
-    RegisterAction g_creatorRegister##className( \
-        #className, (PCreateObject)objectCreator##className)
+#define REGISTER(className)                                                                                                                \
+    className *objectCreator##className() {                                                                                                \
+        return new className;                                                                                                              \
+    }                                                                                                                                      \
+    RegisterAction g_creatorRegister##className(#className, (PCreateObject)objectCreator##className)
 
-class VceCommandsFactory
-{
+class VceCommandsFactory {
 private:
     std::map<std::string, PCreateObject> m_classMap;
     VceCommandsFactory(){};
 
 public:
-    void *CreateObjectByName(std::string className)
-    {
+    void *CreateObjectByName(std::string className) {
         std::map<std::string, PCreateObject>::const_iterator iter;
         iter = m_classMap.find(className);
         if (iter == m_classMap.end())
@@ -35,12 +29,10 @@ public:
         else
             return iter->second(); //函数指针的调用
     }
-    void registClass(std::string name, PCreateObject method)
-    {
+    void registClass(std::string name, PCreateObject method) {
         m_classMap.insert(std::pair<std::string, PCreateObject>(name, method));
     }
-    static VceCommandsFactory &getInstance()
-    {
+    static VceCommandsFactory &getInstance() {
         {
             static VceCommandsFactory cf;
             return cf;
@@ -48,11 +40,9 @@ public:
     }
 };
 
-class RegisterAction
-{
+class RegisterAction {
 public:
-    RegisterAction(std::string className, PCreateObject ptrCreateFn)
-    {
+    RegisterAction(std::string className, PCreateObject ptrCreateFn) {
         VceCommandsFactory::getInstance().registClass(className, ptrCreateFn);
     }
 };
@@ -62,8 +52,7 @@ REGISTER(Command1);
 
 } // namespace vce
 
-int main(int argc, const char *argv[])
-{
+int main(int argc, const char *argv[]) {
     vce::Base *p = NULL;
 
     p = (vce::Base *)vce::VceCommandsFactory::getInstance().CreateObjectByName("Command1");
